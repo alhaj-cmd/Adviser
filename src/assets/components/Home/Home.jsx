e49644113d8ffac4e57e0b2d039993097320a24e
alhaj sarkar <you@domain.com>
2024-04-29 00:13:41 +0600

    home file added

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    // let Counts = 0; 
+    const [Count, setCount] = useState(0);
+
+    const handleCount = () => {
+        setCount(Count+1)
+    }
+    const handleCountMinus = () => {
+        setCount(
+         Count -1
+        )
+    }
     return (
-        <div>
-            <h3 className='w-4'>Hello My Best Friends</h3>
+        <div className='text-center text-3xl mt-6'>
+            <h3>Hello My Best Friends</h3>
+            <h1>Count {Count}</h1>
+            <button onClick={handleCount} className=' btn btn-primary mr-2'>Plus</button>
+            <button onClick={handleCountMinus} className='btn error'>Minus</button>
         </div>
     );
 };
