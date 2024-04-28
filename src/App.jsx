7db74ce852e070bdc5d282a7cfc8703efefd0098
alhaj sarkar <you@domain.com>
2024-04-28 22:55:26 +0600

    Folder and file  create

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Home from './assets/components/Home/Home';
 
 const App = () => {
   return (
@@ -6,6 +7,7 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Home/>
     </div>
   );
 };
---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className='w-4'>Hello My Best Friends</h3>
+        </div>
+    );
+};
+
+export default Home;
