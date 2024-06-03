87c815bffade4ab02375dae9f8af0d125934891d
alhaj sarkar <you@domain.com>
2024-06-03 23:07:16 +0600

    data passing

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -10,12 +10,13 @@ const Home = () => {
     const handleCountMinus = () => {
         setCount(
          Count -1
+         
         )
     }
     return (
         <div className='text-center text-3xl mt-6'>
             <h3>Hello My Best Friends</h3>
-            <h1>Count {Count}</h1>
+            <h1 className='text-bold'>Count {Count}</h1>
             <button onClick={handleCount} className=' btn btn-primary mr-2'>Plus</button>
             <button onClick={handleCountMinus} className='btn error'>Minus</button>
         </div>
