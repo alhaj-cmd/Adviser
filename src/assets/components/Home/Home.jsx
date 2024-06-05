f47e987cb0b91efcabd41ff8176a4fe34c0dfeae
alhaj sarkar <you@domain.com>
2024-06-05 23:25:58 +0600

    count number

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -9,16 +9,17 @@ const Home = () => {
     }
     const handleCountMinus = () => {
         setCount(
-         Count -1
-         
-        )
+    Count-1  
+            
+    )
+
     }
     return (
         <div className='text-center text-3xl mt-6'>
             <h3>Hello My Best Friends</h3>
             <h1 className='text-bold'>Count {Count}</h1>
-            <button onClick={handleCount} className=' btn btn-primary mr-2'>Plus</button>
-            <button onClick={handleCountMinus} className='btn error'>Minus</button>
+            <button onClick={handleCount} className='bg-green-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mr-2'>Plus</button>
+            <button onClick={handleCountMinus} className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'>Minus</button>
         </div>
     );
 };
