7968c7ad92c4a9befb1e82b3b707eef513ef3118
alhaj sarkar <you@domain.com>
2024-06-11 21:33:21 +0600

    filter data

--- src/assets/components/Person/Person.jsx
+++ src/assets/components/Person/Person.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import {Project} from '../../../../public/project';
 
-const Person = () => {
+const Person = () => { 
     const [projectNam, setProject] = useState(Project);
     console.log('given data', projectNam)
+    const handleData = (id) =>{
+        const filterdata = projectNam.filter((d) =>d.id != id);
+        setProject(filterdata) 
+    }
 
 
     return (
-        <div>
-            
+        <div className=' text-center my-4'>
+            {
+                projectNam.map((data) =>( <>
+                <p>{data.name}</p>
+                <button onClick={()=>handleData(data.id)} className='text-xl font-semibold bg-red-200'>Remove</button>
+                </>))
+            }
         </div>
     );
 };
