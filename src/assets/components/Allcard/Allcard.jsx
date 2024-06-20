4cf93bde44b4c6cbf365da9437bdf6ade8c15f5d
alhaj sarkar <you@domain.com>
2024-06-20 22:58:39 +0600

    generate to fetch

---
+++ src/assets/components/Allcard/Allcard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Allcard = (props) => {
+    console.log('All card data', props.setAlldata);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Allcard;
--- src/assets/components/Card/Card.jsx
+++ src/assets/components/Card/Card.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Allcard from '../Allcard/Allcard';
 
 const Card = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(()=>{
+        const loadData = async ()=>{
+            const res = await fetch(
+                `https://openapi.programming-hero.com/api/ai/tools`
+            )
+            const values = await res.json()
+            console.log(values.data.tools);
+            setData(values.data.tools);
+
+        };
+        loadData();
+    },[])
     return (
         <div>
-            
+            {
+                data.map((setAlldata) => {
+                    return <Allcard  setAlldata={setAlldata}/>
+                })
+            }
         </div>
     );
 };
