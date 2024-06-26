757e0313c3dfcf40a41b4fde6960feedf8c08985
alhaj sarkar <you@domain.com>
2024-06-27 00:01:35 +0600

    card section added

--- src/assets/components/Allcard/Allcard.jsx
+++ src/assets/components/Allcard/Allcard.jsx
@@ -5,7 +5,20 @@ const Allcard = (props) => {
     console.log('All card data', props);
     return (
         <div>
-            <p>{description}</p>
+            <div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>{description}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
