39561b3fced1ee82ee57f2db22e0542f049e297e
alhaj sarkar <you@domain.com>
2024-06-26 23:57:15 +0600

    card added

--- src/assets/components/Allcard/Allcard.jsx
+++ src/assets/components/Allcard/Allcard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Allcard = (props) => {
-    console.log('All card data', props.setAlldata);
+    const {description} = props.setAlldata;
+    console.log('All card data', props);
     return (
         <div>
-            
+            <p>{description}</p>
         </div>
     );
 };
